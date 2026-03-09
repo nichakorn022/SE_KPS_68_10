d566631a66de192b97717d9adffe4d9c1a2faa79
Pumpk1n <candylolipop8233@gmail.com>
2026-03-09 20:14:21 +0700

    Login noob

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './frontend/Home.tsx';
 import Shop from './frontend/Shop/ShopHome.jsx';
+import Login from './frontend/Login.jsx'; 
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

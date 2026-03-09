87396780b014b90c722bdab2d8fdca51d2ab16eb
Promthep402 <promthep.t@ku.th>
2026-03-09 20:57:36 +0700

    Event and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './frontend/Home.tsx';
 import Shop from './frontend/Shop/ShopHome.jsx';
+import Event from './frontend/Event/EventPage.tsx';
+import EventDetail from './frontend/Event/Eventdetails.tsx';
+
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:id" element={<EventDetail />} />
       </Routes>
     </BrowserRouter>
   );

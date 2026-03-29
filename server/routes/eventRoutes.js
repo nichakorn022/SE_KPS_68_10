ed8b5faae3ec6272487de62bdd0586278b77d8d1
GuyRwang <jakkrin13110@gmail.com>
2026-03-29 13:55:49 +0700

    Add files via upload

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const adminMiddleware = require("../middleware/adminMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
   getAllEvents,
@@ -8,14 +9,36 @@ const {
   createEvent,
   updateEvent,
   deleteEvent,
-  searchEvents
+  searchEvents,
+  addInterested,
+  removeInterested,
+  checkInterested,
+  getUserInterested,
+  registerEvent,
+  cancelRegistration,
+  checkRegistration
 } = require("../controllers/eventController");
 
+// ---------------- EVENTS ----------------
 router.get("/", getAllEvents);
 router.get("/search", searchEvents);
+
+router.get("/interested/me", authMiddleware, getUserInterested);
+
 router.get("/:id", getEventById);
+
 router.post("/", adminMiddleware, createEvent);
 router.put("/:id", adminMiddleware, updateEvent);
 router.delete("/:id", adminMiddleware, deleteEvent);
+
+// ---------------- INTEREST ----------------
+router.get("/:id/interested/check", authMiddleware, checkInterested);
+router.post("/:id/interested", authMiddleware, addInterested);
+router.delete("/:id/interested", authMiddleware, removeInterested);
+
+// ---------------- REGISTER ----------------
+router.get("/:id/register/check", authMiddleware, checkRegistration);
+router.post("/:id/register", authMiddleware, registerEvent);
+router.delete("/:id/register", authMiddleware, cancelRegistration);
 
 module.exports = router;
